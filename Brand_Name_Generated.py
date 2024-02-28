1c2e4dc7963419b8bc30c6eeb563d8825b2bf77f
Varsha Suma Thota <116201205+varshasuma@users.noreply.github.com>
2024-02-28 14:10:50 +0530

    Create Brand_Name_Generated.py

---
+++ Brand_Name_Generated.py
@@ -0,0 +1,9 @@
+print("Welcome to the Band Name Generator.")
+name = input("whats your name?: \n")
+place = input("venue: \n")
+print(name + " band! " + "venue: " + place)
+print("Welcoming to music world ^ ~ ^ ")
+age = input(" enter your age: ")
+years = 90 - int(age)
+weeks = years * 52
+print(f"You have {weeks} weeks left. Enjoy the golden days with music")
